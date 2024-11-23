fa9fc49f93dce72365f45f9269520ac908360d03
Olgitta <olga.klimkin@gmail.com>
2024-11-23 18:15:58 +0200

    in progress

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../api-spec/spec.json');
 const healthcheckRouter = require('../core/healthcheck/healthcheckRouter');
 
 module.exports = function setUpRoutes(app) {
+    app.use('/api-spec', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     app.use('/', healthcheckRouter);
 }
