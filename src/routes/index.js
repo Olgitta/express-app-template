67688e88cba9f548ad47a676f59ab1ac2513e14f
Olgitta <olga.klimkin@gmail.com>
2024-12-05 14:38:43 +0200

    in progress

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,22 @@
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('../api-spec/spec.json');
 const healthcheckRouter = require('../core/healthcheck/healthcheckRouter');
-const todosRoutes = require('../domains/todos/routes');
 
-module.exports = function setUpRoutes(app) {
+module.exports = async function setUpRoutes(app) {
     app.use('/api-spec', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     app.use('/', healthcheckRouter);
-    app.use('/api/:version/todos', (req, res, next) => {
-        const router = todosRoutes[req.params.version];
+
+    const todosRoutes = await require('../domains/todos/routes');
+    const todosRoutesMap = new Map();
+
+    for (let r in todosRoutes) {
+        todosRoutesMap.set(r, await todosRoutes[r]());
+    }
+
+    app.use('/api/:version/todos', async (req, res, next) => {
+        const router = todosRoutesMap.get(req.params.version);
         if (router) {
-            return router(req, res, next);
+            return await router(req, res, next);
         }
         res.status(400).send('Unsupported API version');
     });
